d0c5cc079c2cf9202d0cbdcc8f0c8a63039cf422
Matthew <35118316+Matt-GitHub@users.noreply.github.com>
2024-07-28 18:11:26 -0500

    db init

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,18 +1,19 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle } from "drizzle-orm/neon-http";
+import { type NeonQueryFunction, neon } from "@neondatabase/serverless";
 
 import { env } from "~/env";
 import * as schema from "./schema";
+
+const sql = neon(process.env.DATABASE_URL!);
+const globalForDb = globalThis as unknown as {
+  conn: NeonQueryFunction<false, false> | undefined;
+};
 
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
-const globalForDb = globalThis as unknown as {
-  conn: postgres.Sql | undefined;
-};
-
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
+const conn = globalForDb.conn ?? sql;
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const db = drizzle(conn, { schema });
